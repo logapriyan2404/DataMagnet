239241aa4f48dcbb43da38c93c48c86c741eff4c
logapriyan2404 <logapriyan.p@hepl.com>
2025-04-17 09:54:28 +0530

    changes

---
+++ src/test/java/com/example/DataMagnet/DataMagnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataMagnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMagnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
